310330d2067c8de81f08e8d4346d2e6e8513a1f9
Mugil Pandian V <MugilPandian@users.noreply.github.com>
2021-01-22 20:32:29 +0530

    Model Database.js file creation

---
+++ Model/database.js
@@ -0,0 +1,8 @@
+const mysql=require('mysql2');
+const connect=mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'root',
+    database:'class',
+    port:'3307'
+});
--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express= require('express');
 var app=express();
 var bodyParser =require('body-parser');
 
-// var connection=require('./model/database'); 
+var connection=require('./model/database.js'); 
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(__dirname));
@@ -17,14 +17,14 @@ app.post('/validate',function(req,res){
     var email=req.body.Email; 
     var pwd=req.body.password;
     console.log("works");
-    // connection.query('select email from student_details where email like ?',[email],(err,results)=>{
-    //     if (err) throw err;
-    //     if(results){
-    //         connection.query('select password from student_details where email like ? and password like ?)',[email,pwd],(err,results)=>{
-    //             res.send('<h3>Hello everyone</h3>');
-    //         })
-    //     }
-    // })
+    connection.query('select email from student_details where email like ?',[email],(err,results)=>{
+        if (err) throw err;
+        if(results){
+            connection.query('select password from student_details where email like ? and password like ?)',[email,pwd],(err,results)=>{
+                res.send('<h3>Hello everyone</h3>');
+            })
+        }
+    })
 
 })
 app.get('/signup',function(req,res){
@@ -40,13 +40,13 @@ var email=req.body.Email;
 var rollno=req.body.rollno;
 var pwd=req.body.password;
 
-// connection.query('insert into student_details values(?,?,?,?)',[Uname,email,rollno,pwd],(err,results)=>{
-//     if(err) throw err;
-//     if(results){
-//         console.log("Values Inserted");
-//         res.sendFile(__dirname +'/views/proj_login.html');
-//     }
-// })
+connection.query('insert into student_details values(?,?,?,?)',[Uname,email,rollno,pwd],(err,results)=>{
+    if(err) throw err;
+    if(results){
+        console.log("Values Inserted");
+        res.sendFile(__dirname +'/views/proj_login.html');
+    }
+})
 
 })
 
