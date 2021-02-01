00d6a9daecf289ea4771088290e14be8a299ea4b
Mugil Pandian V <MugilPandian@users.noreply.github.com>
2021-02-01 19:07:52 +0530

    Password working query v1.2

--- Controller/Routes/route.js
+++ Controller/Routes/route.js
@@ -24,8 +24,8 @@ router.post('/validate',function(req,res){
     connection.query('select email from student_details where email like ?',[email],(err,results)=>{
         if (err) throw err;
         if(results){
-            connection.query('select password from student_details where email like ? and password like ?)',[email,pwd],(err,results1)=>{
-                connection.query('select student_marksheet.*,student_details.email from student_marksheet join student_details on student_marksheet.rollno= student_details.rollno where email like ?',[email],(err,results)=>{
+            connection.query('select pwd from student_details where email like ? and pwd like ?)',[email,pwd],(err,results1)=>{
+                connection.query('select student_marksheet.*,student_details.email from student_marksheet join student_details on student_marksheet.rollno= student_details.rollno where email like ? and pwd like ?',[email,pwd],(err,results)=>{
                     console.log(results);
                     res.render('user-list',{userData:results});
 
