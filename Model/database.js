ea78582314915b2459181cd8e7381c6288942846
Mugil Pandian V <MugilPandian@users.noreply.github.com>
2021-02-04 20:05:51 +0530

    Setting up MongoDB

--- Model/database.js
+++ Model/database.js
@@ -1,10 +1,15 @@
-const mysql=require('mysql2');
-const connect=mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'root',
-    database:'class',
-    port:'3301'
-});
+// const mysql=require('mysql2');
+// const connect=mysql.createConnection({
+//     host:'localhost',
+//     user:'root',
+//     password:'root',
+//     database:'class',
+//     port:'3301'
+// });
 
-module.exports=connect;
+// module.exports=connect;
+
+const {mongoose}=require('mongoose');
+module.exports={
+mongourl:"mongodb+srv://root:root@class.nn36s.mongodb.net/Database?retryWrites=true&w=majority";
+}
